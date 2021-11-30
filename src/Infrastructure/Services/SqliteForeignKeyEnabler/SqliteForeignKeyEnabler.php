512f89682c17c7243b3460a4714a421b14cfae9d
Slawomir Dymitrow <sdymitrow@gmail.com>
2021-11-30 15:42:53 +0100

    cleanup

--- src/Infrastructure/Services/SqliteForeignKeyEnabler/SqliteForeignKeyEnabler.php
+++ src/Infrastructure/Services/SqliteForeignKeyEnabler/SqliteForeignKeyEnabler.php
@@ -6,18 +6,26 @@ use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
 use Doctrine\DBAL\Event\ConnectionEventArgs;
 use Doctrine\DBAL\Events;
 
+/**
+ * SQLite has FK constraints disabled by default for BC.
+ * Enable FK constraints to make sure the app isn't making any funny business in the DB.
+ */
 class SqliteForeignKeyEnabler implements EventSubscriberInterface
 {
+    /**
+     * @return array
+     */
     public function getSubscribedEvents(): array
     {
         return [Events::postConnect];
     }
 
+    /**
+     * @param ConnectionEventArgs $args
+     * @throws \Doctrine\DBAL\Exception
+     */
     public function postConnect(ConnectionEventArgs $args): void
     {
-        if (strtolower($args->getConnection()->getDatabasePlatform()->getName()) !== 'sqlite') {
-            return;
-        }
         $args->getConnection()->executeStatement('PRAGMA foreign_keys = ON;');
     }
 }
